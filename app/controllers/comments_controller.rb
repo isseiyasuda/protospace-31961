1a615a9442a74289cf7d6a65cd85079d42436b59
isseiyasuda <iseyasuda0520@icloud.com>
2021-03-12 18:20:39 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    
+    @comment = Comment.new(comment_params)
+    
+    if @comment.save
+      
+      redirect_to prototype_path(@comment.prototype_id)
+    else
+      render "prototypes/show(@comment.prototype_id)"
+    end
+  end
+
+  #def show
+    #@comments = @prototype.comments.includes(:user)
+  #end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id:current_user.id,prototype_id:params[:prototype_id])
+  end
+end
